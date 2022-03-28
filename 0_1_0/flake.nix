4617afba7da3a08a6c110718e577a082ca2af12a
hugosenari <hugosenari@gmail.com>
2022-03-27 21:50:05 -0300

    chore: re index gnutls

--- 0_1_0/flake.nix
+++ 0_1_0/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-gnutls-0_1_0.ref   = "refs/tags/0.1.0";
   inputs.src-gnutls-0_1_0.owner = "FedericoCeratto";
   inputs.src-gnutls-0_1_0.repo  = "nim-gnutls";
-  inputs.src-gnutls-0_1_0.dir   = "";
   inputs.src-gnutls-0_1_0.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
